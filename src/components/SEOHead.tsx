8774a6c7d26ebbc335fe7e1cd1888f044807e4af
rogerrunner <81278320+rogerrunner@users.noreply.github.com>
2026-03-18 12:41:54 -0700

    Add SEOHead component placeholder

---
+++ src/components/SEOHead.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function SEOHead() {
+  return (
+    <>
+      <title>Ingenieria Bernal</title>
+      <meta name="description" content="Default SEO head placeholder" />
+    </>
+  );
+}
